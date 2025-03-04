8c1ae5c6c4b190785725503b51da807f94d07c8c
Wiktor Lutowski <wlutowski@s201.local>
2025-03-04 12:06:13 +0100

    Add employee class

---
+++ Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DaneXML
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public decimal Wages { get; set; }
+        public DateTime EmploymentDate { get; set; }
+        public DateTime? DismissalDate { get; set; }
+    }
+}
+
