73320354425cdbee6db4728b90010db2e2688eb8
Wiktor Lutowski <wlutowski@s201.local>
2025-03-04 12:15:52 +0100

    Conenect class with datagrid"

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.ObjectModel;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Xml.Serialization;
 
 namespace DaneXML
 {
@@ -20,9 +9,24 @@ namespace DaneXML
     /// </summary>
     public partial class MainWindow : Window
     {
+        ObservableCollection<Employee> employees;
         public MainWindow()
         {
             InitializeComponent();
+
+            employees = new ObservableCollection<Employee>();
+
+            dataGrid.ItemsSource = employees;
+        }
+
+        private void saveButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void loadButton_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
